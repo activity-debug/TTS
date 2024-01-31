60a664aa939fbed90514fe2d4a158a676863e2c6
activity-debug <rendrapcx@hotmail.com>
2024-02-01 06:27:10 +0700

    add other way for banner ads in scan act

--- app/src/main/java/com/rendrapcx/tts/ui/BarcodeActivity.kt
+++ app/src/main/java/com/rendrapcx/tts/ui/BarcodeActivity.kt
@@ -11,16 +11,25 @@ import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore.Images.Media
+import android.util.DisplayMetrics
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.PopupMenu
 import androidx.core.net.toUri
 import androidx.lifecycle.lifecycleScope
 import com.daimajia.androidanimations.library.Techniques
 import com.daimajia.androidanimations.library.YoYo
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdSize
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.RequestConfiguration
 import com.google.zxing.BinaryBitmap
 import com.google.zxing.LuminanceSource
 import com.google.zxing.MultiFormatReader
@@ -34,6 +43,7 @@ import com.rendrapcx.tts.constant.Const.Companion.isEditor
 import com.rendrapcx.tts.constant.Const.Companion.isEnableClick
 import com.rendrapcx.tts.constant.RequestCode
 import com.rendrapcx.tts.databinding.ActivityBarcodeBinding
+import com.rendrapcx.tts.helper.GoogleMobileAdsConsentManager
 import com.rendrapcx.tts.helper.Helper
 import com.rendrapcx.tts.helper.MPlayer
 import com.rendrapcx.tts.helper.Sora
@@ -47,6 +57,7 @@ import java.io.File
 import java.io.FileInputStream
 import java.io.InputStream
 import java.util.Base64
+import java.util.concurrent.atomic.AtomicBoolean
 
 
 class BarcodeActivity : AppCompatActivity() {
@@ -60,6 +71,31 @@ class BarcodeActivity : AppCompatActivity() {
     private var qrListLevel = mutableListOf<Data.Level>()
     private var qrListQuestion = mutableListOf<Data.Question>()
 
+    private val isMobileAdsInitializeCalled = AtomicBoolean(false)
+    private val initialLayoutComplete = AtomicBoolean(false)
+    private lateinit var adView: AdView
+    private lateinit var googleMobileAdsConsentManager: GoogleMobileAdsConsentManager
+
+    // Determine the screen width (less decorations) to use for the ad width.
+    // If the ad hasn't been laid out, default to the full screen width.
+    private val adSize: AdSize
+        get() {
+            val display = windowManager.defaultDisplay
+            val outMetrics = DisplayMetrics()
+            display.getMetrics(outMetrics)
+
+            val density = outMetrics.density
+
+            var adWidthPixels = binding.adViewContainer.width.toFloat()
+            if (adWidthPixels == 0f) {
+                adWidthPixels = outMetrics.widthPixels.toFloat()
+            }
+
+            val adWidth = (adWidthPixels / density).toInt()
+            return AdSize.getCurrentOrientationAnchoredAdaptiveBannerAdSize(this, adWidth)
+        }
+
+
     @RequiresApi(Build.VERSION_CODES.R)
     @SuppressLint("ClickableViewAccessibility", "SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -68,7 +104,54 @@ class BarcodeActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         Helper().apply { hideSystemUI() }
+
+        adView = AdView(this)
+        binding.adViewContainer.addView(adView)
+
+        // Log the Mobile Ads SDK version.
+        Log.d("JACK", "Google Mobile Ads SDK Version: " + MobileAds.getVersion())
+
+        googleMobileAdsConsentManager = GoogleMobileAdsConsentManager.getInstance(applicationContext)
+        googleMobileAdsConsentManager.gatherConsent(this) { error ->
+            if (error != null) {
+                // Consent not obtained in current session.
+                Log.d("JACK", "${error.errorCode}: ${error.message}")
+            }
+
+            if (googleMobileAdsConsentManager.canRequestAds) {
+                initializeMobileAdsSdk()
+            }
+
+            if (googleMobileAdsConsentManager.isPrivacyOptionsRequired) {
+                // Regenerate the options menu to include a privacy setting.
+                invalidateOptionsMenu()
+            }
+        }
+
+        // This sample attempts to load ads using consent obtained in the previous session.
+        if (googleMobileAdsConsentManager.canRequestAds) {
+            initializeMobileAdsSdk()
+        }
+
+        binding.adViewContainer.viewTreeObserver.addOnGlobalLayoutListener {
+            if (!initialLayoutComplete.getAndSet(true) && googleMobileAdsConsentManager.canRequestAds) {
+                loadBanner()
+            }
+        }
+        // Set your test devices. Check your logcat output for the hashed device ID to
+        // get test ads on a physical device. e.g.
+        // "Use RequestConfiguration.Builder().setTestDeviceIds(Arrays.asList("ABCDEF012345"))
+        // to get test ads on this device."
+        MobileAds.setRequestConfiguration(
+            RequestConfiguration.Builder().setTestDeviceIds(listOf("ABCDEF012345")).build()
+        )
+
+
+
         myClipboard = getSystemService(CLIPBOARD_SERVICE) as ClipboardManager?
+
+
+
 
         binding.textResultContent.text = ""
         binding.editPaste.setText("")
@@ -174,6 +257,77 @@ class BarcodeActivity : AppCompatActivity() {
 
         }
 
+    }
+
+    override fun onPause() {
+        adView.pause()
+        super.onPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        adView.resume()
+    }
+
+    override fun onDestroy() {
+        adView.destroy()
+        super.onDestroy()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.action_menu, menu)
+        val moreMenu = menu?.findItem(R.id.action_more)
+        moreMenu?.isVisible = googleMobileAdsConsentManager.isPrivacyOptionsRequired
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val menuItemView = findViewById<View>(item.itemId)
+        PopupMenu(this, menuItemView).apply {
+            menuInflater.inflate(R.menu.popup_menu, menu)
+            show()
+            setOnMenuItemClickListener { popupMenuItem ->
+                when (popupMenuItem.itemId) {
+                    R.id.privacy_settings -> {
+                        // Handle changes to user consent.
+                        googleMobileAdsConsentManager.showPrivacyOptionsForm(this@BarcodeActivity) { formError ->
+                            if (formError != null) {
+                                Toast.makeText(this@BarcodeActivity, formError.message, Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                        true
+                    }
+                    else -> false
+                }
+            }
+            return super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun loadBanner() {
+        // This is an ad unit ID for a test ad. Replace with your own banner ad unit ID.
+        adView.adUnitId = Const.bannerCaAppId
+        adView.setAdSize(adSize)
+
+        // Create an ad request.
+        val adRequest = AdRequest.Builder().build()
+
+        // Start loading the ad in the background.
+        adView.loadAd(adRequest)
+    }
+
+    private fun initializeMobileAdsSdk() {
+        if (isMobileAdsInitializeCalled.getAndSet(true)) {
+            return
+        }
+
+        // Initialize the Mobile Ads SDK.
+        MobileAds.initialize(this) {}
+
+        // Load an ad.
+        if (initialLayoutComplete.get()) {
+            loadBanner()
+        }
     }
 
     private fun saveQRToDB() {
